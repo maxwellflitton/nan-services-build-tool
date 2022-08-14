d57f12ada3f49e92acde6a0268e2f3414cc14f3c
maxwell flitton <maxwellflitton@maxwells-MacBook-Pro.local>
2022-08-14 12:44:00 +0100

    basic github run working

---
+++ scripts/cross_build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $SCRIPTPATH
+
+cargo install cross --git https://github.com/cross-rs/cross
+
+git clone https://github.com/cross-rs/cross
+
+cd cross
+git submodule update --init --remote
+cd ..
+
+rm -rf cross
+
+cd ..
+cd build_tool
+
+# https://github.com/cross-rs/cross#supported-targets
+cross build --target x86_64-unknown-linux-musl --release
+cross build --target aarch64-apple-darwin --release
+cross build --target x86_64-apple-darwin --release
+
+# rm -r releases
+# mkdir releases
+
+# cp ./target/aarch64-apple-darwin/release/build_tool ./releases/build_tool_aarch64_apple_darwin
+# cp ./target/x86_64-unknown-linux-musl/release/build_tool ./releases/build_tool_x86_64_unknown_linux_musl
+# cp ./target/x86_64-apple-darwin/release/build_tool ./releases/build_tool_x86_64_apple_darwin
